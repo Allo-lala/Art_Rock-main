3ffa9f7a1d89a3bedf34b0a3b41e1e34a60fb04c
Allan Kyagulanyi <allankyagulanyi8@gmail.com>
2023-11-24 17:16:43 +0300

    footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 (function ($) {
     "use strict";
 
@@ -47,7 +45,21 @@
         return false;
     });
 
-
+    const trendingSwiper = new Swiper(".trending__swiper", {
+      loop: true,
+      slidesPerView: "auto",
+      spaceBetween: 50,
+    });
+    
+    const testimonialSwiper = new Swiper(".testimonial__swiper", {
+      loop: true,
+      spaceBetween: 30,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         items: 1,
@@ -64,3 +76,17 @@
     
 })(jQuery);
 
+const trendingSwiper = new Swiper(".trending__swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 50,
+});
+
+const testimonialSwiper = new Swiper(".testimonial__swiper", {
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
